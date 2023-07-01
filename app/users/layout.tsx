43864fa26530f5d3dec4597f73e0a91c82cdea5e
Abhijit Pandey <abhijitpandey524@gmail.com>
2023-07-01 15:17:52 +0530

    message body created

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,21 +1,21 @@
-import Sidebar from '../components/sidebar/Sidebar';
-import UserList from './components/UserList';
+import getUsers from "../actions/getUsers";
+import Sidebar from "../components/sidebar/Sidebar";
+import UserList from "./components/UserList";
 
 export default async function UsersLayout({
-    children
-} : {
-    children : React.ReactNode;
+  children
+}: {
+  children: React.ReactNode,
 }) {
-    return(
-        <Sidebar>
-            <div className="h-screen">
-                <UserList items={users} />    
-            
-                {children}
-            
-            
-            </div> 
-        </Sidebar>
-        
-    )
+  const users = await getUsers();
+
+  return (
+    
+    <Sidebar>
+      <div className="h-full">
+        <UserList items={users} />
+        {children}
+      </div>
+    </Sidebar>
+  );
 }
